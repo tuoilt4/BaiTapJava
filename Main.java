f71024c543123e3d351fbf422ca83f1c07a94464
tuoilt4 <84076866+tuoilt4@users.noreply.github.com>
2021-05-14 17:38:29 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,11 +10,57 @@ public class Main {
         System.out.printf("Giai thừa của " + m + " theo vòng lặp do là: %d\n", baiTapBuoi4.tinhGtWhile(m));
 
         System.out.print("Liệt kê các số nguyên tố nhỏ hơn 100 theo vòng lặp for: ");
-        System.out.print(baiTapBuoi4.ChuoiSnt1(100));
+        for (int i = 3; i < 100; i++) {
+            if (baiTapBuoi4.checkSnt(i))
+            {
+                System.out.print(i+"\t");
+            }
+        }
+        System.out.print("\nLiệt kê các số nguyên tố nhỏ hơn 100 theo vòng lặp while: ");
+        int k=3;
+        while (k<100)
+        {
+        if (baiTapBuoi4.checkSnt(k))
+            {
+                System.out.print(k+"\t");
+            }
+        k++;
+        }
 
-        System.out.print("Nhập số lượng số nguyên tố cần liệt kê:");
-        int k = scanner.nextInt();
-        System.out.print(baiTapBuoi4.ChuoiSnt2(k));
+        System.out.print("\nLiệt kê các số nguyên tố nhỏ hơn 100 theo vòng lặp do: ");
+        int p=3;
+        do{
+        if (baiTapBuoi4.checkSnt(p))
+        {
+            System.out.print(p +"\t");
+        }
+        p++;}
+        while (p<100);
+
+        System.out.print("\nNhập số lương số nguyên tố đầu tiên cần liệt kê: ");
+        int h = scanner.nextInt();
+        System.out.print("Liệt kê theo vòng lặp for: ");
+        int dem=0;
+        for (int i = 2; dem < h; i++) {
+            if (baiTapBuoi4.checkSnt(i))
+            {
+                System.out.print(i+"\t");
+                dem ++;
+            }
+        }
+
+        System.out.print("\nLiệt kê theo vòng lặp while: ");
+        int q = 2;
+        int dem1 =0;
+        while(dem1 < h)
+        {
+            if (baiTapBuoi4.checkSnt(q))
+            {
+                System.out.print(q+"\t");
+                dem1 ++;
+            }
+            q++;
+        }
 
 
 
